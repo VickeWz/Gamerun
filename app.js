bbbbcae0909901a4f9912a6b5c47b3530eb0820f
VickeWz <117863134+VickeWz@users.noreply.github.com>
2023-11-10 09:46:10 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) { // Change 'isInteracting' to 'isIntersecting'
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); // Fix the assignment here
